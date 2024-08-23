b1eea3a5a5bf1d9556c17a812de4686afabb7e65
Ali Gholami Rudi <ali@rudi.ir>
2024-08-23 10:39:18 +0330

    cell.sh: allow overlay root directory

--- cell.sh
+++ cell.sh
@@ -9,28 +9,38 @@ ROOTFS="/path/to/rootfs.tar.gz"
 ROOTCP="/more/root/files/dir"
 
 root="$1"
-if test "$#" != "1"; then
-	echo "usage: $0 root"
+lower="$2"
+if test -z "$root"; then
+	echo "usage: $0 root [lower]"
 	exit 1
 fi
 if test -d "$root" -o -f $root; then
-	echo "cell.sh: directory $root already exists"
+	echo "$0: directory $root already exists"
 	exit 1
 fi
-# copy rootfs
+# prepare root directory
 mkdir $root
-cd $root
-tar xf $ROOTFS
-test -d "$ROOTCP" && cp -r $ROOTCP/* $root/
-test -d $root/foe && chown -R 99:99 $root/foe
-# fill /etc
-echo "foe:x:99:99:foe:/foe:/bin/mksh" >>$root/etc/passwd
-echo "foo::99:" >>$root/etc/group
-echo "foe:x:0::::::" >>$root/etc/shadow
-echo "nameserver 4.2.2.4" >>$root/etc/resolv.conf
+if test -z "$lower"; then
+	cd $root
+	tar xf $ROOTFS
+	test -d "$ROOTCP" && cp -r $ROOTCP/* $root/
+	test -d $root/foe && chown -R 99:99 $root/foe
+	# fill /etc
+	echo "foe:x:99:99:foe:/foe:/bin/mksh" >>$root/etc/passwd
+	echo "foo::99:" >>$root/etc/group
+	echo "foe:x:0::::::" >>$root/etc/shadow
+	echo "nameserver 4.2.2.4" >>$root/etc/resolv.conf
+else
+	mkdir $root.tmp
+fi
 # cell options
 echo '#!/bin/sh' >$root.sh
-echo "root=\"\${ROOT+ -R$root -c00405f9 -p0 -g0}\"" >>$root.sh
-echo "exec $CELL -r$root $OPTS \$root \"\$@\"" >>$root.sh
+if test -z "$lower"; then
+	echo "base=\"$root\"" >>$root.sh
+else
+	echo "base=\"$root:$lower:$root.tmp\"" >>$root.sh
+fi
+echo "root=\"\${ROOT+ -R\$base -c00405f9 -p0 -g0}\"" >>$root.sh
+echo "exec $CELL -r\$base $OPTS \$root \"\$@\"" >>$root.sh
 chmod 755 $root
 chmod 700 $root.sh
